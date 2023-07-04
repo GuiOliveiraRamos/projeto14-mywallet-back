6b84296f3e4e76f39b0917fb1b1d83d33e280267
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-07-04 10:10:39 -0300

    inicio do projeto

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db();
+    console.log("rodando");
+  })
+  .catch((err) => console.log(err.message));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
