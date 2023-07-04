0d5d37100315f17e746e6c0e66f8b4cf29855aa5
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-07-04 10:32:37 -0300

    backend cadastro

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
 
 const app = express();
 app.use(cors());
@@ -18,6 +19,34 @@ mongoClient
     console.log("rodando");
   })
   .catch((err) => console.log(err.message));
+
+const schemaSignInUp = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.any().required().min(3),
+  confirmPassword: Joi.any().valid(Joi.ref("password")).required(),
+});
+
+app.post("/cadastro", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  try {
+    const validation = schemaSignInUp.validate(
+      { name, email, password, confirmPassword },
+      { abortEarly: false }
+    );
+    if (validation.error) {
+      return res.status(422).send(validation.error);
+    }
+    const alreadySigned = await db.collection("users").findOne({ email });
+    if (alreadySigned) {
+      return res.sendStatus(409);
+    }
+
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
